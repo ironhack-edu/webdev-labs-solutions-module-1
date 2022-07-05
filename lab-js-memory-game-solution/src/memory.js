aa1ab4bc5f8b6ce71797e17bd5fc1b2a03288aa1
ross-u <cirkovic.uros.srb@gmail.com>
2022-07-05 18:10:35 +0200

    Update LAB "JS Memory Game" solution

--- lab-js-memory-game-solution/src/memory.js
+++ lab-js-memory-game-solution/src/memory.js
@@ -7,7 +7,12 @@ class MemoryGame {
     this.pairsClicked = 0;
     this.shuffleCards();
   }
+
   shuffleCards() {
+    if (!this.cards) {
+      return undefined;
+    };
+
     let len = this.cards.length;
     while (len > 0) {
       len--;
@@ -16,23 +21,46 @@ class MemoryGame {
       this.cards[len] = this.cards[rdmInd];
       this.cards[rdmInd] = temp;
     }
+
+    return this.cards;
   }
+
   checkIfPair(card1, card2) {
     this.pairsClicked++;
     if (card1 === card2) {
       this.pairsGuessed++;
-      this.isFinished();
       return true;
     }
     return false;
   }
-  isFinished() {
-    if (this.pairsGuessed === 2) {
-      document.querySelector("#memory_board").innerHTML = "";
-      let h1 = document.createElement("h1");
-      h1.style.color = "pink";
-      h1.innerHTML = "YOU WON!!!";
-      document.querySelector("#memory_board").appendChild(h1);
+
+  checkIfFinished() {
+
+    const totalPairs = this.cards.length / 2;
+    if (this.pairsGuessed === totalPairs) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
+
+// class MemoryGame {
+//   constructor(cards) {
+//     this.cards = cards;
+//     // add the rest of the class properties here
+//   }
+
+//   shuffleCards() {
+//     // ... write your code here
+//   }
+
+//   checkIfPair(card1, card2) {
+//     // ... write your code here
+//   }
+
+//   checkIfFinished() {
+//     // ... write your code here
+//   }
+// }
+
