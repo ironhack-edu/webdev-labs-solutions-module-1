d91dcc98b4eae7cf081c7e1c7677bbb0571e7e85
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2024-01-16 12:33:17 +0100

    feat(mini project): update solution for question.js

--- mini-project-javascript-quiz-solution/src/question.js
+++ mini-project-javascript-quiz-solution/src/question.js
@@ -7,22 +7,31 @@ class Question {
   }
 
   shuffleChoices() {
-    if (!this.choices || !this.choices.length) {
-      return undefined;
+    for (let i = 0; i < this.choices.length; i++) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      
+      const temp = this.choices[i];
+      this.choices[i] = this.choices[randomIndex];
+      this.choices[randomIndex] = temp;
     }
-
-    const shuffledChoices = [];
-    let last = this.choices.length;
-
-    while (last > 0) {
-      last--;
-      const randomIndex = Math.floor(Math.random() * last);
-      const randomChoice = this.choices[randomIndex];
-      shuffledChoices.push(randomChoice);
-      this.choices.splice(randomIndex, 1);
-    }
-
-    this.choices = shuffledChoices;
-    return this.choices;
   }
+  // // Another way to do it:
+  // shuffleChoices() {
+  //   if (!this.choices || !this.choices.length) {
+  //     return undefined;
+  //   }
+
+  //   const shuffledChoices = [];
+  //   let last = this.choices.length;
+
+  //   while (last > 0) {
+  //     last--;
+  //     const randomIndex = Math.floor(Math.random() * last);
+  //     const randomChoice = this.choices[randomIndex];
+  //     shuffledChoices.push(randomChoice);
+  //     this.choices.splice(randomIndex, 1);
+  //   }
+
+  //   this.choices = shuffledChoices;
+  // }
 }
